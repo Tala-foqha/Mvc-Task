7424dac50b5dc63e508e046093e08d1cde3233cf
tala <foqhat835@gmail.com>
2026-01-09 00:03:50 +0200

    Add MVC project files

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,43 @@
+﻿using CategoryMVCTask.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CategoryMVCTask.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        public ViewResult Index()
+        {
+            var products = context.Categories;
+            return View("Home", products
+            );
+
+        }
+        public ViewResult Details(int Id)
+        {
+            var categories = context.Categories.Find(Id);
+            return View("Details", categories);
+        }
+
+        public ViewResult Delete(int Id)
+        {
+            var categories = context.Categories.Find(Id);
+            context.Categories.Remove(categories);
+            context.SaveChanges();
+            var category = context.Categories;
+            return View("Home", category);
+        }
+
+        public ViewResult Create()
+        {
+            return View("Create"
+            );
+        }
+        public ViewResult Store(Category request)
+        {
+            context.Categories.Add(request);
+            context.SaveChanges();
+            return View("Create");
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CategoryMVCTask.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.;database=mvc18;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+    }
